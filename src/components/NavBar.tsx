e24a2ad56a5d2a1b355fc2d560aefd5ade8c9f13
amaben2020 <60056927+amaben2020@users.noreply.github.com>
2024-10-21 11:50:22 -0500

    Complete

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 const NavBar = ({ component }: { component: React.ReactElement }) => {
   return (
-    <div className="bg-blue h-[123px] mx-auto flex items-center justify-center shadow-right shadow-[0px_4px_6px_0px_rgba(0,_0,_0,_0.3)]">
+    <div className="bg-blue h-[123px] mx-auto flex items-center justify-center  shadow-[0px_4px_6px_0px_rgba(0,_0,_0,_0.3)]">
       {component}
     </div>
   );
